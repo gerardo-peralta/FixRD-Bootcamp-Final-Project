92562fbae819c9cef37523c5691c7c4f8e4ec0c1
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-11-19 17:43:56 -0400

    Types and interfaces

--- core/src/types.ts
+++ core/src/types.ts
@@ -1,7 +1,39 @@
+export type role = "client" | "technician";
+
 export interface User {
   id: number;
   name: string;
   email: string;
+  password: string;
+  role: role;
+  createdAt: Date;
+}
+
+export interface Technician {
+  userId: string;
+  categories: string[];
+  pricePerHour: number;
+  description: string;
+  location: string;
+  photo: string;
+  rating: number;
+}
+
+export type status = "pending" | "accepted" | "completed" | "cancelled";
+export interface Request {
+  clientId: string;
+  technicianId: string;
+  description: string;
+  date: Date;
+  status: status;
+}
+
+export interface Review {
+  requestId: string;
+  technicianId: string;
+  clientId: string;
+  rating: number;
+  comment: string;
 }
 
 export interface ApiResponse<T> {
